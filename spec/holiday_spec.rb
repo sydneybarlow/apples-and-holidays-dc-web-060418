88bfa76543d3d5b2cfa377365ed8dbdf2aad9f49
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-03 03:13:12 +0000

    Done.

--- spec/holiday_spec.rb
+++ spec/holiday_spec.rb
@@ -48,12 +48,6 @@ describe "holiday_supplies_hash" do
       expect(memorial_day_supplies).to include("Grill")
       expect(memorial_day_supplies).not_to include("Table Cloth")
     end
-
-    it "adds a different supply to memorial day" do
-      add_supply_to_memorial_day(holiday_supplies, "Table Cloth")
-      expect(memorial_day_supplies).to include("Table Cloth")
-      expect(memorial_day_supplies).not_to include("Grill")
-    end
   end
 
   # Question 4
